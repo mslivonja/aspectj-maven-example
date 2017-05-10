9a612f258901e69947681934bd5b83d6abf95ede
eslimat <matija.slivonja@ericsson.com>
2017-05-10 14:55:27 +0200

    Added new Bar class and bar method invocation.

--- src/main/java/li/barlog/asjex/Foo.java
+++ src/main/java/li/barlog/asjex/Foo.java
@@ -1,7 +1,10 @@
 package li.barlog.asjex;
 
+import org.slivonja.Bar;
+
 public class Foo {
     public void foo() {
         System.out.println("foo");
+		new Bar().bar();
     }
 }
---
+++ src/main/java/org/slivonja/Bar.java
@@ -0,0 +1,10 @@
+package org.slivonja;
+
+/**
+ * Created by eslimat on 10.5.2017..
+ */
+public class Bar {
+	public void bar() {
+		System.out.println("bar");
+	}
+}
